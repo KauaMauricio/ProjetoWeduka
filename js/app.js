683a48fa2a0b92434c43754dcf0fd51e4b90c81b
KauaMauricio <kauaktdmauricio1@gmail.com>
2024-03-07 20:22:51 -0300

    teste

--- js/app.js
+++ js/app.js
@@ -13,3 +13,18 @@ btnSignup.addEventListener("click", function () {
     body.className = "sign-up-js";
 })
 
+
+function verificarLetras() {
+    // Obtém o valor do input
+    let inputValor = document.getElementById("letras").value;
+
+    // Expressão regular para verificar se contém somente letras
+    let regex = /^[A-Za-z]+$/;
+
+    // Verifica se o valor corresponde à expressão regular
+    if (regex.test(inputValor)) {
+        document.getElementById("resultado").textContent = "Login realizado com sucesso.";
+    } else {
+        document.getElementById("resultado").textContent = "O campo "/nome/" contém caracteres não permitidos.";
+    }
+}
